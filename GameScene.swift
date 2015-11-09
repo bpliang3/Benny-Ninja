1a83ec8365d0dfd22e719247f0c368720189e46a
Ben Liang <bpliang3@illinois.edu>
2015-11-09 17:27:01 -0600

    Init

---
+++ GameScene.swift
@@ -0,0 +1,33 @@
+//
+//  GameScene.swift
+//  Benny Ninja
+//
+//  Created by Ben Liang on 11/9/15.
+//  Copyright (c) 2015 Ben Liang. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var movingGround: BLMovingGround!
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        backgroundColor = UIColor(red: 159.0/255.0, green: 201.0/255.0, blue: 244.0/255.0, alpha: 1.0)
+        
+        movingGround = BLMovingGround(size: CGSizeMake(view.frame.width, 20))
+        movingGround.position = CGPointMake(0, view.frame.size.height/2)
+        addChild(movingGround)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        movingGround.start()
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ GameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  GameViewController.swift
+//  Benny Ninja
+//
+//  Created by Ben Liang on 11/9/15.
+//  Copyright (c) 2015 Ben Liang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class GameViewController: UIViewController {
+    
+    var scene: GameScene!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure our view
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the Scene
+        skView.presentScene(scene)
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
